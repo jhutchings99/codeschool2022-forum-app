968f05349ae15b04a1437cef324e1b4871547a50
Jeremy Hutchings <79938993+jhutchings99@users.noreply.github.com>
2022-06-29 12:10:55 -0600

    got stuff set up

--- app.js
+++ app.js
@@ -1,6 +1,121 @@
+const URL = "https://forum2022.codeschool.cloud";
+
 var app = new Vue({
     el: '#app',
     data: {
-        test: "message"
+        currentPage: "login-page",
+
+        // Login form inputs
+        loginEmailInput: "",
+        loginPasswordInput: "",
+
+        // Signup form inputs
+        newEmailInput: "",
+        newPasswordInput: "",
+        newFullNameInput: "",
     },
+    methods: {
+        // GET /session - Ask server if we are logged in
+        getSession: async function () {
+            let response = await fetch(`${URL}/session`, {
+                method: "GET",
+                credentials: "include"
+            });
+
+            // Check if logged in
+            if (response.status == 200) {
+                // logged in
+                console.log("logged in");
+                let data = await response.json();
+                console.log(data);
+            } else if (response.status == 401) {
+                // not logged in
+                console.log("not logged in");
+                let data = await response.json();
+                console.log(data);
+            } else {
+                console.log("error GETTING /session", response.status, response);
+            }
+        },
+        // POST /session - Attempt to login
+        postSession: async function () {
+            let loginCredentials = {username: this.loginEmailInput, password: this.loginPasswordInput}
+            let response = await fetch(`${URL}/session`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(loginCredentials)
+            }); 
+
+            // Parse response body
+            let body = response.json();
+            console.log(body);
+
+            // Check if login was successful
+            if (response.status == 201) {
+                console.log("Successful login attempt");
+
+                // Take user to new page and clear inputs
+                this.currentPage = "home-page";
+                this.loginEmailInput = "";
+                this.loginPasswordInput = "";
+
+            } else if (response.status == 401) {
+                console.log("Unsuccessful login attemp")
+
+                // Let user know login failed and clear password input
+                alert("Unsuccessful login");
+                this.loginPasswordInput = "";
+            } else {
+                console.log("error POSTING /session", response.status, response);
+            }
+        },
+        // POST /users - Create new user
+        postUser: async function () {
+            let newUser = {
+                username: this.newEmailInput,
+                password: this.newPasswordInput,
+                fullname: this.newFullNameInput
+            }
+            let response = await fetch(`${URL}/user`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newUser)
+            }); 
+
+            // Parse response body
+            let body = response.json();
+            console.log(body);
+
+            // Check if user was created
+            if (response.status == 201) {
+                console.log("Successful user creation");
+
+                // Take user to new page and clear inputs
+                this.currentPage = "login-page";
+                this.newEmailInput = "";
+                this.newPasswordInput = "";
+                this.newFullNameInput = "";
+
+            } else if (response.status == 400) {
+                console.log("Unsuccessful user creation");
+
+                // Let user know user creation failed and clear inputs
+                alert("Unsuccessful user creation");
+                this.newEmailInput = "";
+                this.newPasswordInput = "";
+                this.newFullNameInput = "";
+            } else {
+                console.log("error POSTING /users", response.status, response);
+            }
+        }
+    },
+    created: function () {
+        this.getSession();
+    }
 });
